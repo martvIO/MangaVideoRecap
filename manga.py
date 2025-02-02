cc28f30c58be7cbaadcf30e071925b82b1985cac
martvIO <136852336+martvIO@users.noreply.github.com>
2025-02-02 14:57:04 +0200

    updated 2

--- manga.py
+++ manga.py
@@ -15,20 +15,5 @@ def ai(chapter_pages, character_bank):
     character_bank["images"] = [read_image(x) for x in character_bank["images"] if isinstance(x, str)]
 
     with torch.no_grad():
-        per_page_results = model.do_chapter_wide_prediction(chapter_pages, character_bank, use_tqdm=True, do_ocr=True)
-        logger.debug(f"per_page_results: {per_page_results}")
-    transcript = []
-    for i, (image, page_result) in enumerate(zip(chapter_pages, per_page_results)):
-        model.visualise_single_image_prediction(image, page_result, f"page_{i}.png")
-        speaker_name = {
-            text_idx: page_result["character_names"][char_idx] for text_idx, char_idx in page_result["text_character_associations"]
-        }
-        for j in range(len(page_result["ocr"])):
-            if not page_result["is_essential_text"][j]:
-                continue
-            name = speaker_name.get(j, "unsure") 
-            transcript.append(f"<{name}>: {page_result['ocr'][j]}")
-    with open(f"transcript.txt", "w") as fh:
-        for line in transcript:
-            fh.write(line + "\n")    
+        per_page_results = model.do_chapter_wide_prediction(chapter_pages, character_bank, use_tqdm=True, do_ocr=True)  
     return per_page_results
